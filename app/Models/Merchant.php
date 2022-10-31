d7330538e8e37a7ac7012cb3eeb17886a9f0cc6d
Gleb <gleb0ponomarev@gmail.com>
2022-10-31 03:52:28 +0300

    create Merchant model

---
+++ app/Models/Merchant.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Merchant
+ *
+ * @mixin Builder
+ */
+class Merchant extends Model
+{
+
+}
