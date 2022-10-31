1c186b1e51bc9e8cee74f6fa45353fb41120c877
Gleb <gleb0ponomarev@gmail.com>
2022-10-31 03:52:08 +0300

    adding two routes in api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('payment/first-payment-gateway', [PaymentController::class, 'firstPaymentGateway']);
+Route::post('payment/second-payment-gateway', [PaymentController::class, 'secondPaymentGateway']);
