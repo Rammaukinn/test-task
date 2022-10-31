1699c2eaf3eb4fbd56c430cebef77da59e726a9d
Gleb <gleb0ponomarev@gmail.com>
2022-10-31 03:54:34 +0300

    created PaymentController

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\PaymentGateway\FirstPaymentGateway;
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    public function firstPaymentGateway(Request $request)
+    {
+        $payment_gateway = new FirstPaymentGateway($request->all());
+        $validate_errors = $payment_gateway->validateData();
+
+        if ($validate_errors->isNotEmpty()) return response()->json($validate_errors, 403);
+
+        if(!$payment_gateway->checkSignature()) {
+            return response()->json(
+                [
+                    "sign" => [
+                        "Authentication failed."
+                    ]
+                ],
+                401
+            );
+        }
+
+        $payment_gateway->writeToBase();
+
+        return response()->json([[]]);
+    }
+
+    public function secondPaymentGateway(Request $request)
+    {
+
+    }
+}
