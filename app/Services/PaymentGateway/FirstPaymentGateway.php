86d2b5020a04aa342791a627c54d35e4db5ec583
Gleb <gleb0ponomarev@gmail.com>
2022-10-31 23:03:16 +0300

    added new statuses in validator added mutate status

--- app/Services/PaymentGateway/FirstPaymentGateway.php
+++ app/Services/PaymentGateway/FirstPaymentGateway.php
@@ -16,6 +16,7 @@ class FirstPaymentGateway implements PaymentGateway
 
     public function __construct(array $request_data)
     {
+        ksort($request_data);
         $this->data = collect($request_data);
     }
 
@@ -24,7 +25,7 @@ class FirstPaymentGateway implements PaymentGateway
         $validator = Validator::make($this->data->all(), [
             'merchant_id' => 'required|integer|exists:merchants',
             'payment_id' => 'required|integer',
-            'status' => 'required|in:new,completed',
+            'status' => 'required|in:new,pending,completed,expired,rejected',
             'amount' => 'required|integer',
             'amount_paid' => 'required|integer',
             'timestamp' => 'required|integer',
@@ -37,20 +38,32 @@ class FirstPaymentGateway implements PaymentGateway
     public function checkSignature(): bool
     {
         $filtered = $this->data->except(['sign']);
-        $merchant_id = $this->data->get("merchant_id");
+        $merchant_id = $this->data->get('merchant_id');
         $merchant_key = Merchant::where('merchant_id', $merchant_id)->first('merchant_key')->merchant_key;
         $joint = $filtered->join(':', $merchant_key);
         $hash = hash('sha256', $joint);
 
-        return $hash == $this->data->get("sign");
+        return $hash == $this->data->get('sign');
+    }
+
+    private function mutateStatus(): string
+    {
+        $status = $this->data->get('status');
+        $mutations = [
+            'new' => 'created'
+        ];
+
+        return array_key_exists($status, $mutations) ? $mutations[$status] : $status;
     }
 
     public function writeToBase()
     {
+        $this->data['status'] = $this->mutateStatus();
 
         Payment::updateOrCreate(
             [
-                'merchant_id' => $this->data->get('merchant_id')
+                'merchant_id' => $this->data->get('merchant_id'),
+                'payment_id' => $this->data->get('payment_id')
             ],
             $this->data->except(['sign'])->all()
         );
